8af426816d0fff7c7b285ce8803c853bf7788af5
Omar Garcia <omar.garcia@omargl.net>
2023-08-09 11:28:30 -0600

    variables

---
+++ js-react/01_variables/index.js
@@ -0,0 +1,12 @@
+// Variables
+console.log(miVariable)
+
+var miVariable
+
+miVariable = 'variable'
+
+let variable = 'variable creada con let'
+
+const constante = 'este valor es constante'
+
+console.log(miVariable, variable)
